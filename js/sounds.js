808aebd5fccf9da60d20a818b97e09e0fe683536
nikadezinkin1 <127156280+nikadezinkin1@users.noreply.github.com>
2023-05-30 14:45:13 -0700

    LKLJ

--- js/sounds.js
+++ js/sounds.js
@@ -6,7 +6,7 @@ sounds.forEach(sound => {
     btn.innerText = sound
     btn.addEventListener('click', () => {
         stopSongs()
-        document.getElementById(sound).play
+        document.getElementById(sound).play()
     })
     document.getElementById('buttons').appendChild(btn)
 })
