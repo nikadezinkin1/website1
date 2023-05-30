ffcbf661b7653ae4ec336392175adcf727c65094
nikadezinkin1 <127156280+nikadezinkin1@users.noreply.github.com>
2023-05-30 12:52:26 -0700

    Update website

---
+++ js/colorboard.js
@@ -0,0 +1,20 @@
+const container = document.getElementById('container')
+const colors = ['orange', 'yellow', 'white', 'cornflowerblue', 'lightskyblue' ]
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+}
+
+function setColor(element) {
+    const color = colors[Math.floor(Math.random() * 5)]
+    element.style.background = color
+}
+
+function removeColor(element) {
+    element.style.background = '#1f1f1f'
+}
---
+++ js/typing.js
@@ -0,0 +1,19 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = "We Love Programming!!!"
+let idx = 1
+let speed = 300 /speedEl.value
+speedEl.addEventListener('input', (element) => {
+    speed = 300 / element.target.value
+})
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0,idx)
+    idx++
+    if (idx > text.length){
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
